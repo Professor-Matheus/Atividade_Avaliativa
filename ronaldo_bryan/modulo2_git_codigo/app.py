3cd3acb06f8296d4a1dbd534c4e31deb1f89b6e0
Ronaldo <00001101375164sp@al.educacao.sp.gov.br>
2026-09-15 12:08:04 -0300

    Modulo 2 Alterado

--- ronaldo_bryan/modulo2_git_codigo/app.py
+++ ronaldo_bryan/modulo2_git_codigo/app.py
@@ -1,14 +1,9 @@
-# 💻 Módulo 2: Exercício Prático de Código (Python)
-# REGRA: Você deve implementar as funções abaixo e realizar NO MÍNIMO 3 COMMITS separados no Git, 
-# cada um cobrindo uma funcionalidade ou correção específica.
-
 def calcular_velocidade(sprints):
     """
     1. Função para calcular média de Story Points finalizados.
     sprints: lista de inteiros contendo os pontos de cada Sprint (ex: [12, 10, 14])
     """
-    # TODO: Implementar lógica de soma e média
-    pass
+    return sum(sprints) / len(sprints)
 
 
 def validar_dependencia(tarefa_pai_concluida):
@@ -16,8 +11,7 @@ def validar_dependencia(tarefa_pai_concluida):
     2. Função para validar se a dependência técnica foi concluída.
     tarefa_pai_concluida: booleano (True ou False)
     """
-    # TODO: Retornar True se concluída, False caso contrário
-    pass
+    return tarefa_pai_concluida
 
 
 def status_release(sprints_estimadas):
@@ -25,5 +19,4 @@ def status_release(sprints_estimadas):
     3. Função para formatar o status da release.
     sprints_estimadas: número inteiro indicando a quantidade de Sprints
     """
-    # TODO: Retornar string no formato "Release prevista para X Sprints"
-    pass
+    return f"Release prevista para {sprints_estimadas} Sprints"
