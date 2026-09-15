c1bba88836a925a1eede7ed73dcb2b6ad6c3ee0e
Teste <teste@mail.com>
2026-09-14 21:49:45 -0300

    Perguntas respondidas modulo2

--- teste/modulo2_git_codigo/app.py
+++ teste/modulo2_git_codigo/app.py
@@ -7,7 +7,7 @@ def calcular_velocidade(sprints):
     1. Função para calcular média de Story Points finalizados.
     sprints: lista de inteiros contendo os pontos de cada Sprint (ex: [12, 10, 14])
     """
-    # TODO: Implementar lógica de soma e média
+    # TODO: Implementar lógica de soma e médiaaaaaa aa
     pass
 
 
@@ -16,7 +16,7 @@ def validar_dependencia(tarefa_pai_concluida):
     2. Função para validar se a dependência técnica foi concluída.
     tarefa_pai_concluida: booleano (True ou False)
     """
-    # TODO: Retornar True se concluída, False caso contrário
+    # TODO: Rvalidaçao boleanaada
     pass
 
 
@@ -25,5 +25,5 @@ def status_release(sprints_estimadas):
     3. Função para formatar o status da release.
     sprints_estimadas: número inteiro indicando a quantidade de Sprints
     """
-    # TODO: Retornar string no formato "Release prevista para X Sprints"
+    # TODO: ReEstimativas
     pass
--- teste/modulo2_git_codigo/perguntas_teoricas.md
+++ teste/modulo2_git_codigo/perguntas_teoricas.md
@@ -4,8 +4,9 @@ Responda às questões abaixo diretamente neste arquivo.
 
 1. **O que caracteriza um "Commit Atômico" e por que commits gigantescos dificultam a integração e a rastreabilidade do código?**
 
-   *Sua resposta aqui:*
+   *Sua resposta aqui:* aaaaa
 
 2. **Como ocorrem os conflitos de merge no Git e qual a melhor estratégia para resolvê-los de forma segura antes da integração?**
 
-   *Sua resposta aqui:*
+   *Sua resposta aqui:* aaaa
+ 
